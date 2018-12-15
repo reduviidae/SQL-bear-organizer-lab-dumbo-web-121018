df80bc34591d386c575b0461aec9ad031510d21d
reduviidae <wiljago@gmail.com>
2018-12-15 15:18:27 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,127 @@
+INSERT INTO bears (
+  name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+) VALUES (
+  "Mr. Chocolate",
+  12,
+  "female",
+  "brown",
+  "chill",
+  1
+);
+
+INSERT INTO bears (
+  name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+) VALUES (
+  "Rowdy",
+  8,
+  "male",
+  "white",
+  "rowdy",
+  1
+);
+
+INSERT INTO bears (
+  name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+) VALUES (
+  "Tabitha",
+  9,
+  "male",
+  "brown",
+  "sleepy",
+  1
+);
+
+INSERT INTO bears (
+  name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+) VALUES (
+  "Melissa",
+  11,
+  "female",
+  "black",
+  "chill",
+  1
+);
+
+INSERT INTO bears (
+  name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+) VALUES (
+  "Grinch",
+  10,
+  "female",
+  "black",
+  "nice",
+  1
+);
+
+INSERT INTO bears (
+  name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+) VALUES (
+  "Wendy",
+  15,
+  "female",
+  "black",
+  "sleepy",
+  1
+);
+
+INSERT INTO bears (
+  name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+) VALUES (
+  "Sergeant Brown",
+  14,
+  "female",
+  "brown",
+  "stern",
+  0
+);
+
+INSERT INTO bears (
+  name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+) VALUES (
+  NULL,
+  11,
+  "male",
+  "brown",
+  "murderous",
+  1
+);
